20bc48a1d23bed5baa00c09fe40f79f808815ba1
Michael Hage <mhage222@gmail.com>
2024-12-03 00:51:42 -0500

    finished day 3

--- src/main/java/hagem/aoc2024/Main.java
+++ src/main/java/hagem/aoc2024/Main.java
@@ -1,16 +1,24 @@
 package hagem.aoc2024;
 
-import hagem.aoc2024.day2.Day2;
+import hagem.aoc2024.day3.Day3;
 
 public class Main {
     public static void main(String[] args) {
+
+        long startTime = System.nanoTime();
 
 //        Day 1
 //        System.out.println(new Day1().run());
 
 //        Day 2
-//        System.out.println(new Day2Part1().run());
-        System.out.println(new Day2().run());
+//        System.out.println(new Day2().run());
+
+//        Day 3
+        System.out.println(new Day3().run());
+
+        long endTime = System.nanoTime();
+
+        System.out.println("Execution Time: " + ( (endTime - startTime) / 1000000d) + "ms");
 
     }
 }
---
+++ src/main/java/hagem/aoc2024/day3/Day3.java
@@ -0,0 +1,72 @@
+package hagem.aoc2024.day3;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+
+
+public class Day3 {
+
+    File file = new File(Objects.requireNonNull(getClass().getResource("/Day3.data.txt")).getFile());
+
+    public String run() {
+
+//        PART 1
+//        Pattern pattern = Pattern.compile("mul\\([0-9]+,[0-9]+\\)");
+
+//        PART 2
+        Pattern pattern = Pattern.compile("(mul\\([0-9]+,[0-9]+\\)|(do\\(\\)|don't\\(\\)))");
+
+        long total = 0;
+        boolean enabler = true;
+
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(file)) ) {
+            String line;
+            while( (line = reader.readLine()) != null ) {
+
+                List<MatchResult> results = pattern
+                                    .matcher(line)
+                                    .results()
+                                    .toList();
+
+                for (MatchResult result : results) {
+
+                    String match = result.group(0);
+
+                    if(match.startsWith("don't")) {
+                        enabler = false;
+                        continue;
+
+                    } else if (match.startsWith("do")) {
+                        enabler = true;
+                        continue;
+
+                    }
+
+                    if(enabler) {
+                        match = match.replaceAll("[^0-9,]", "");
+
+                        String[] matchArr = match.split(",");
+
+                        total += Long.parseLong(matchArr[0]) * Long.parseLong(matchArr[1]);
+                    }
+
+                }
+
+            }
+
+        } catch(IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+
+        return "" + total;
+    }
+
+}
